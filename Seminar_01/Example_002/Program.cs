ff9c62327d4c85c871193e43fc1d75858c7d7579
Karina <kamironova09@gmail.com>
2023-01-30 16:15:15 +0300

    kvadrat chisla

---
+++ Seminar_01/Example_002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine($"Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Второе число: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == (b * b))
+{
+    Console.WriteLine($"УраУра!{a} квадрат {b}");
+}
+else
+{
+    Console.WriteLine("Не квадрат!");
+}

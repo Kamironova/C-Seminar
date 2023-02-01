51818d4060228250e4884301c6187527e8c29721
Karina <kamironova09@gmail.com>
2023-02-01 14:49:05 +0300

    example 004

---
+++ Seminar_01/Example_004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача: Программа должна показать все целые числа от минуса введеного числа до плюса
+
+Console.Clear();
+
+System.Console.Write("--> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = -1 * a;
+
+while (i <= a)
+{
+    System.Console.Write(i + " ");
+    i++;
+};

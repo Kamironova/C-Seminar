447bc4981306a4bb3f48a435f0158fda0b7dcf0f
Карина Миронова <kamironova@192.168.1.9>
2023-02-08 17:34:49 +0300

    example 007

---
+++ Seminar_02/Example_007/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+System.Console.WriteLine("Enter number 1 -->");
+double a = Convert.ToUInt64(Console.ReadLine());
+
+System.Console.WriteLine("Enter number 2 -->");
+double b = Convert.ToInt64(Console.ReadLine());
+
+if (a % b == 0)
+{
+    System.Console.WriteLine($"{a} кратно {b}");
+}
+
+else 
+{
+    double c = (a % b) % 10;
+    System.Console.WriteLine($"Не кратно, остаток от деления равен {c}");
+}

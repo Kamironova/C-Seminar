30176e1cdacf494192ab22cf4a56eea44fe03e5c
Карина Миронова <kamironova@192.168.1.9>
2023-02-08 14:23:57 +0300

    commit02

---
+++ Seminar_02/Example_005/Program.cs
@@ -0,0 +1,20 @@
+﻿int Max(int a, int b)
+{
+    if (a > b) return a;
+    else return b;
+}
+
+
+Random rnd = new Random();
+
+int a = rnd.Next(10, 100);
+
+System.Console.WriteLine($"Random = {a}");
+
+int firstNumber = a / 10;
+int secondNumber = a % 10;
+
+int max = Max(firstNumber, secondNumber);
+
+System.Console.WriteLine($"Max = {max}");
+
---
+++ Seminar_02/Example_006/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Random rnd = new Random();
+
+int a = rnd.Next(100, 1000);
+
+Console.WriteLine($"Random Number {a}");
+
+int b = ((a / 100) * 10);
+
+
+int c = (a % 10);
+
+System.Console.WriteLine(b+c);
+
+

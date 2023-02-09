5eebc4b41f9d0b79867dec8fb4227c71ae1f17dd
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-09 13:36:43 +0300

    Example 008 function

--- Seminar_02/Example_008/Program.cs
+++ Seminar_02/Example_008/Program.cs
@@ -3,8 +3,21 @@
 
 Console.Clear();
 
-System.Console.WriteLine("Enter -->");
-double a = Convert.ToDouble(Console.ReadLine());
+// Функция запрашивающая число на ввод от пользователя:
+
+double Number()
+{
+    System.Console.WriteLine("-->");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+
+
+
+
+//System.Console.WriteLine("Enter -->");
+//double a = Convert.ToDouble(Console.ReadLine());
 
 // if (a % 7 == 0)
 // {
@@ -21,11 +34,14 @@ double a = Convert.ToDouble(Console.ReadLine());
 // {
 //     System.Console.WriteLine(a + " не кратно одновременно 7 и 23");
 // }
-if ((a % 7 == 0) && (a % 23 == 0))
+
+double num = Number();
+
+if ((num % 7 == 0) && (num % 23 == 0))
 {
-    System.Console.WriteLine(a + " кратно одновременно 7 и 23");
+    System.Console.WriteLine(num + " кратно одновременно 7 и 23");
 }
 else
 {
-    System.Console.WriteLine(a + " не кратно одновременно 7 и 23");
+    System.Console.WriteLine(num + " не кратно одновременно 7 и 23");
 }

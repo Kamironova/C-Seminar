1d9b3ceec4f55c7dde3ad8d9b6470c5fff8ba2d1
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-09 14:05:57 +0300

    Example 009

---
+++ Seminar_02/Example_009/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программуб которая на вход принимает 2 числа и проверяет
+// является ли одно квадратом другого
+
+
+int Number()
+{
+    System.Console.Write("Enter --> ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int a = Number();
+int b = Number();
+
+if (a * a == b || b * b == a)
+{
+    System.Console.WriteLine(a + " является квадратом " + b);
+}
+
+else
+{
+    System.Console.WriteLine(a + " не является квадратом " + b);
+
+}

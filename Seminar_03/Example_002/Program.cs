eb116b94dc83513e2ec9903393cf933a6d493b11
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-10 11:44:50 +0300

    example 02

---
+++ Seminar_03/Example_002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает
+// таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+
+System.Console.Write("--> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    System.Console.Write($" {i * i}");
+}
+
+System.Console.WriteLine(); // это команда нужна потому что в цикле стоит знак "$"

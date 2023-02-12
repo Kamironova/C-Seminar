cdcfadcab4fa1a8712cc58d9cdd6f831b4fafb28
Карина Миронова <kamironova@192.168.1.9>
2023-02-12 15:40:12 +0300

    sem 04, exam 02

---
+++ Seminar_04/Example_02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу которая принимает на вход число 
+// и выдает количество цифр в числе
+
+
+Console.Clear();
+
+System.Console.Write("--> ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+while (a > 0)
+{
+    a = a / 10;
+    i++;
+}
+System.Console.WriteLine("Количество символов " + i);
+

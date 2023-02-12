f6bb5e4ae9c36842b11499d67e2990a171118377
Карина Миронова <kamironova@192.168.1.9>
2023-02-12 12:35:59 +0300

    sem 04, exam 01

---
+++ Seminar_04/Example_01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Нипишите программу, которая принимает на вход число "а"
+// и выдает сумму чисел от 1 до "а"
+
+Console.Clear();
+
+System.Console.Write("--> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 1; i <= a; i++)
+{
+    sum += i;
+}
+
+System.Console.WriteLine("Sum = " + sum);

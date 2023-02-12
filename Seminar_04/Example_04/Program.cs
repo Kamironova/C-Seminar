3895446f0bebc4c3bdaadcbbaec34ea7514be4ab
Карина Миронова <kamironova@192.168.1.9>
2023-02-12 17:00:03 +0300

    sem 04, exam 04

---
+++ Seminar_04/Example_04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу которая выводит массив из 8 элементов, заполненный
+// нулями и еденицами в случайном порядке. Напр: [0,1,1,0,0,1,0,1]
+
+Console.Clear();
+
+int [] myArray = new int [7];
+
+Random rand = new Random();
+    for (int x = 0; x < myArray.Length; x++)
+    {
+        myArray[x] = rand.Next(2);
+        Console.WriteLine(myArray[x]);
+    }

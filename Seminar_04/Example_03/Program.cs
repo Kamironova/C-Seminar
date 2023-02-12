7165d1587ba23588a0e5a1fb105af9e649234513
Карина Миронова <kamironova@192.168.1.9>
2023-02-12 15:55:23 +0300

    sem 04, exam 03

---
+++ Seminar_04/Example_03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает произведение чисел от 1 до N
+
+Console.Clear();
+
+System.Console.Write("--> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int f = 1;
+
+for (int i = 1; i <= a; i++)
+{
+    f = f * i;
+}
+
+System.Console.WriteLine("Произведение из {a} -> " +f);

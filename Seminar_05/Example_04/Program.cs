3a3145de908397385bc5a5d3a51dad12b127d562
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-15 12:37:28 +0300

    example 04/05

---
+++ Seminar_05/Example_04/Program.cs
@@ -0,0 +1,25 @@
+﻿//задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+
+Console.Clear(); 
+int[] array = new int[123];
+
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(124);
+    System.Console.Write($"[{array[i] + "]"}");
+}
+System.Console.WriteLine();
+
+int count = 0;
+
+foreach (var item in array)
+{
+    if (item >= 10 && item <= 99)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine(count);
---
+++ Seminar_05/Example_05/Program.cs
@@ -0,0 +1,24 @@
+﻿// найдите произведение пар чисел в одномерном масиве.
+//Парой считем первый и последний элемент, второй и предпоследний и т.д. 
+// Результат напишите в новом массиве
+
+Console.Clear();
+
+System.Console.WriteLine("-->");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[len];
+
+Random rnd = new Random();
+for (int i = 0; i < len; i++)
+{
+    array[i] = rnd.Next(1,10);
+    System.Console.Write($"{array[i]},");
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < len/2; i++)
+{
+    System.Console.WriteLine(array[i]*array[len-1-i]);
+}
+if ((len%2!=0)) System.Console.WriteLine(array[len/2]);

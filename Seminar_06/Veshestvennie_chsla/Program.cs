c0544bc3e84b6251f24d6724ea186cf6ab9cce85
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-16 12:49:59 +0300

    Вещественные

---
+++ Seminar_06/Veshestvennie_chsla/Program.cs
@@ -0,0 +1,26 @@
+﻿// Как сгенерировать вещественные числа, так чтобы они полоучалистб дробными 
+// а не целыми
+
+double[] arr = new double[10];
+
+void FillArray(double[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = genRndDouble(-100, 100, rnd);
+    }
+}
+
+FillArray(arr);
+
+foreach (var item in arr)
+{
+    System.Console.WriteLine(item);
+}
+
+double genRndDouble(int a, int b, Random rnd)
+{
+    double rndDouble = a + rnd.NextDouble() * (b - a);
+    return rndDouble;
+}
